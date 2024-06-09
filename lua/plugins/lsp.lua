3118564f547ccebaf9b6cbeee387be2e217accf3
林汉青 <linhanqing01943@os-easy.com>
2024-06-09 18:59:13 +0800

    :bug: 解决不存在的 jsregex

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,7 +42,6 @@ return {
         'L3MON4D3/LuaSnip',
         lazy = true,
         dependencies = { 'rafamadriz/friendly-snippets' },
-        build = "make install_jsregex",
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load({ path = { "./my_snippets" } })
         end
