5ff7e4b7bddf3d39d949b2ac4e282ab3959b19d9
林汉青 <linhanqing01943@os-easy.com>
2024-06-09 23:55:48 +0800

    常规更新

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,8 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        event = 'VeryLazy',
-        config = function (_, opts)
+        event = 'BufReadPre',
+        config = function(_, opts)
             -- 高级选项，巨幅提升 parser 下载速度
             -- 要求 curl, tar, 且可以在非 admin 下创建 SymbolicLink
             -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#how-will-the-parser-be-downloaded
@@ -45,7 +45,7 @@ return {
     },
     {
         'brenoprata10/nvim-highlight-colors',
-        ft = {'vue', 'css', 'less', 'html', 'javascriptreact'},
+        ft = { 'vue', 'css', 'less', 'html', 'javascriptreact' },
         opts = {
             render = 'virtual',
             enable_named_colors = false,
