6b7143bc103f37b388647d239005ba0bb2b16fd8
林汉青 <linhanqing01943@os-easy.com>
2024-06-11 23:35:35 +0800

    :art: 垃圾电脑性能优化

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,28 +5,28 @@
 
 return {
     {
-        'kevinhwang91/nvim-ufo',
-        event = 'VeryLazy', -- 不能为 BufReadPre，见  https://github.com/kevinhwang91/nvim-ufo/issues/47#issuecomment-1248773096
+        "kevinhwang91/nvim-ufo",
+        event = "VeryLazy", -- 不能为 BufReadPre，见  https://github.com/kevinhwang91/nvim-ufo/issues/47#issuecomment-1248773096
         config = function()
-            local ufo = require('ufo')
+            local ufo = require("ufo")
 
-            vim.o.foldcolumn = '0'
+            vim.o.foldcolumn = "0"
             vim.o.foldlevel = 99
             vim.o.foldlevelstart = 99
             vim.o.foldenable = true
 
-            vim.keymap.set('n', 'zR', ufo.openAllFolds)
-            vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+            vim.keymap.set("n", "zR", ufo.openAllFolds)
+            vim.keymap.set("n", "zM", ufo.closeAllFolds)
 
             ufo.setup()
         end,
         dependencies = {
-            { 'kevinhwang91/promise-async' }
-        }
+            { "kevinhwang91/promise-async" },
+        },
     },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
         lazy = true,
         init = function()
             -- Disable automatic setup, we are doing it manually
@@ -35,31 +35,31 @@ return {
         end,
     },
     {
-        'williamboman/mason.nvim',
-        config = true
+        "williamboman/mason.nvim",
+        config = true,
     },
     {
-        'L3MON4D3/LuaSnip',
+        "L3MON4D3/LuaSnip",
         lazy = true,
-        dependencies = { 'rafamadriz/friendly-snippets', 'saadparwaiz1/cmp_luasnip' },
+        dependencies = { "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip" },
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
             -- require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./my_snippets" } })
-        end
+        end,
     },
     {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
         dependencies = {
-            { 'onsails/lspkind.nvim' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            'L3MON4D3/LuaSnip',
+            { "onsails/lspkind.nvim" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            "L3MON4D3/LuaSnip",
         },
         config = function()
-            local lsp_zero = require('lsp-zero')
-            local lspkind = require('lspkind')
-            local cmp = require('cmp')
+            local lsp_zero = require("lsp-zero")
+            local lspkind = require("lspkind")
+            local cmp = require("cmp")
             local cmp_action = lsp_zero.cmp_action() --Tab 自动完成之类的
 
             lsp_zero.extend_cmp()
@@ -67,23 +67,23 @@ return {
             cmp.setup({
                 formatting = {
                     format = lspkind.cmp_format({
-                        mode = 'symbol_text', -- show only symbol annotations
-                        maxwidth = 50,        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        mode = "symbol_text", -- show only symbol annotations
+                        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                         -- can also be a function to dynamically calculate max width such as
                         -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                        show_labelDetails = true
-                    })
+                        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        show_labelDetails = true,
+                    }),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<Tab>'] = cmp_action.luasnip_supertab(),
-                    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                    ["<Tab>"] = cmp_action.luasnip_supertab(),
+                    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+                    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
                 }),
                 window = {
                     completion = cmp.config.window.bordered(),
@@ -91,48 +91,51 @@ return {
                 },
                 snippet = {
                     expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 sources = {
                     { name = "nvim_lsp" },
-                    { name = "buffer", 
+                    {
+                        name = "buffer",
                         option = {
                             get_bufnrs = function()
                                 local bufs = {}
                                 for _, win in ipairs(vim.api.nvim_list_wins()) do
-                                    local buf = vim.api.nvim_win_get_buf(win) 
+                                    local buf = vim.api.nvim_win_get_buf(win)
                                     -- 限制大于 1M 的文件
-                                    if vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf)) <= 1024 * 1024 then
+                                    if
+                                        vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+                                        <= 1024 * 256
+                                    then
                                         bufs[buf] = true
                                     end
                                 end
                                 return vim.tbl_keys(bufs)
-                            end
-                        }
+                            end,
+                        },
                     },
                     { name = "path" },
                     { name = "nvim_lsp_signature_help" },
-                    { name = "luasnip" }
+                    { name = "luasnip" },
                 },
                 expirment = {
-                    ghost_text = true
-                }
+                    ghost_text = true,
+                },
             })
-        end
+        end,
     },
     {
-        'williamboman/mason-lspconfig.nvim',
+        "williamboman/mason-lspconfig.nvim",
         lazy = true,
         config = function()
-            local lsp_zero = require('lsp-zero')
+            local lsp_zero = require("lsp-zero")
 
             -- 一个简单的工具函数
-            local registry = require('mason-registry')
+            local registry = require("mason-registry")
             local get_mason_path = function(package)
                 return registry.get_package(package):get_install_path()
             end
-
 
             -- 必须在设置各种 lsp 之前调用，所以放这里
             lsp_zero.extend_lspconfig()
@@ -142,33 +145,33 @@ return {
                     textDocument = {
                         foldingRange = {
                             dynamicRegistration = false,
-                            lineFoldingOnly = true
-                        }
-                    }
-                }
+                            lineFoldingOnly = true,
+                        },
+                    },
+                },
             })
 
-            require('mason-lspconfig').setup({
+            require("mason-lspconfig").setup({
                 ensure_installed = {},
                 -- 启用 lsp 自动配置
                 handlers = {
                     function(server_name)
-                        require('lspconfig')[server_name].setup({})
+                        require("lspconfig")[server_name].setup({})
                     end,
 
                     -- 覆盖 lua
                     lua_ls = function()
                         local lua_opts = lsp_zero.nvim_lua_ls()
-                        require('lspconfig').lua_ls.setup(lua_opts)
+                        require("lspconfig").lua_ls.setup(lua_opts)
                     end,
                     powershell_es = function()
-                        require('lspconfig').powershell_es.setup({
-                            bundle_path = get_mason_path('powershell-editor-services')
+                        require("lspconfig").powershell_es.setup({
+                            bundle_path = get_mason_path("powershell-editor-services"),
                         })
                     end,
                     vtsls = function()
                         -- 考虑安装 nvim-vtsls 插件
-                        require('lspconfig')['vtsls'].setup({
+                        require("lspconfig")["vtsls"].setup({
                             filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
                             settings = {
                                 vtsls = {
@@ -176,8 +179,12 @@ return {
                                         globalPlugins = {
                                             {
                                                 name = "@vue/typescript-plugin",
-                                                location = vim.fs.joinpath(get_mason_path('vue-language-server'),
-                                                    'node_modules', '@vue', 'language-server'),
+                                                location = vim.fs.joinpath(
+                                                    get_mason_path("vue-language-server"),
+                                                    "node_modules",
+                                                    "@vue",
+                                                    "language-server"
+                                                ),
                                                 languages = { "vue" },
                                                 configNamespace = "typescript",
                                                 enableForWorkspaceTypeScriptVersions = true,
@@ -185,25 +192,25 @@ return {
                                         },
                                     },
                                 },
-                            }
+                            },
                         })
-                    end
-                }
+                    end,
+                },
             })
-        end
+        end,
     },
     {
-        'neovim/nvim-lspconfig',
-        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-        event = { 'BufReadPre', 'BufNewFile' },
+        "neovim/nvim-lspconfig",
+        cmd = { "LspInfo", "LspInstall", "LspStart" },
+        event = { "BufReadPre", "BufNewFile" },
         dependencies = {
-            'williamboman/mason-lspconfig.nvim',
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lsp-signature-help' }
+            "williamboman/mason-lspconfig.nvim",
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lsp-signature-help" },
         },
         config = function()
             -- This is where all the LSP shenanigans will live
-            local lsp_zero = require('lsp-zero')
+            local lsp_zero = require("lsp-zero")
 
             --- if you want to know more about lsp-zero and mason.nvim
             --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
@@ -212,12 +219,12 @@ return {
                 -- see :help lsp-zero-keybindings
                 -- to learn the available actions
                 lsp_zero.default_keymaps({ buffer = bufnr })
-                vim.keymap.set('n', '<up>', vim.diagnostic.goto_prev, { silent = true })
-                vim.keymap.set('n', '<down>', vim.diagnostic.goto_next, { silent = true })
-                vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { silent = true })
-                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { silent = true })
+                vim.keymap.set("n", "<up>", vim.diagnostic.goto_prev, { silent = true })
+                vim.keymap.set("n", "<down>", vim.diagnostic.goto_next, { silent = true })
+                vim.keymap.set("n", "gh", vim.lsp.buf.hover, { silent = true })
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { silent = true })
             end)
-        end
+        end,
     },
     {
         "folke/trouble.nvim",
@@ -255,5 +262,5 @@ return {
                 desc = "Quickfix List (Trouble)",
             },
         },
-    }
+    },
 }
