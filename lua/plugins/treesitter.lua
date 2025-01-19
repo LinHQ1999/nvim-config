2199ca2f877106a41a98ec584384d71f2c721ee9
LinHQ <Hanqing_Lin1999@outlook.com>
2025-01-19 22:47:35 +0800

    更多加载 Treesitter 情况

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         event = 'BufReadPre',
+        cmd = { 'TSInstallInfo', 'TSInstall' },
         config = function(_, opts)
             -- 高级选项，巨幅提升 parser 下载速度
             -- 要求 curl, tar, 且可以在非 admin 下创建 SymbolicLink
