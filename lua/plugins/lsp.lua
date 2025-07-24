825916423e2401fd9715c593f48eff484b788b7f
LinHQ <Hanqing_Lin1999@outlook.com>
2025-07-24 15:42:54 +0800

    feat: 恢复自定义 snippet 功能

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,7 +39,7 @@ return {
         },
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
-            -- require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./my_snippets" } })
+            require("luasnip.loaders.from_snipmate").lazy_load({ paths = { "./my_snippets" } })
         end,
     },
     {
