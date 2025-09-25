7e1a87ba4151b7d0d010e792f3050e24d3d1f6f9
LinHQ1999 <Hanqing_Lin1999@outlook.com>
2025-09-25 21:19:35 +0800

    feat!: 切到 main nvim-treesitter 分支

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,76 +1,82 @@
+local LazyTreesitter = { "VeryLazy", "BufReadPost", "BufNewFile" }
+
 return {
     {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-        cmd = { 'TSInstallInfo', 'TSInstall' },
-        init = function(plugin)
-            -- HACK: https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/plugins/treesitter.lua#L21-L29
-            require("lazy.core.loader").add_to_rtp(plugin)
-            require("nvim-treesitter.query_predicates")
-        end,
-        opts = {
-            ensurse_installed = {
-                "go", "typescript", "tsx", "html", "http", "javascript", "styled", "jsdoc", "json", "vue", "yaml",
-                "toml"
-            },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = { "autohotkey" },
-            },
-            indent = {
-                enable = true,
-                disable = {
-                    "javascript",
-                    "typescript"
-                }
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ao"] = "@block.outer",
-                        ["io"] = "@block.inner",
-                        ["av"] = "@assignment.outer",
-                        ["iv"] = "@assignment.inner",
-                    },
-                    selection_modes = {
-                        ['@function.outer'] = 'V',
-                        ['@block.outer'] = 'V',
-                    },
-                    include_surrounding_whitespace = true,
-                },
-            }
-        },
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = false, -- 根据文档，不推荐懒加载
+        branch = "main",
         config = function(_, opts)
-            -- 高级选项，巨幅提升 parser 下载速度
-            -- 要求 curl, tar, 且可以在非 admin 下创建 SymbolicLink
-            -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Windows-support#how-will-the-parser-be-downloaded
-            require("nvim-treesitter.install").prefer_git = false
+            local ts = require("nvim-treesitter")
 
-            require("nvim-treesitter.configs").setup(opts)
+            local lang = { "go", "typescript", "tsx", "html", "http", "javascript", "styled", "jsdoc", "json", "vue",
+                "yaml", "toml", }
+            ts.install(lang)
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = require("handmade").lang2ft(lang),
+                callback = function(e)
+                    vim.treesitter.start(e.buf)
+                    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+
+            ts.setup(opts)
         end,
-        dependencies = {
-            { 'nvim-treesitter/nvim-treesitter-context',    opts = {} },
-            { 'nvim-treesitter/nvim-treesitter-textobjects' }
-        }
     },
     {
-        'windwp/nvim-ts-autotag',
-        ft = { 'vue', 'html', 'typescriptreact', 'javascriptreact', 'xml' },
+        "nvim-treesitter/nvim-treesitter-context",
+        event = LazyTreesitter,
         opts = {}
     },
     {
-        'brenoprata10/nvim-highlight-colors',
-        ft = { 'vue', 'css', 'less', 'html', 'typescriptreact', 'javascriptreact', 'dart' },
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = LazyTreesitter,
+        branch = "main",
         opts = {
-            render = 'virtual',
+            select = {
+                enable = true,
+                lookahead = true,
+                selection_modes = {
+                    ['@function.outer'] = 'V',
+                    ['@block.outer'] = 'V',
+                },
+                include_surrounding_whitespace = true,
+            },
+        },
+        config = function(_, opts)
+            local keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ao"] = "@block.outer",
+                ["io"] = "@block.inner",
+                ["av"] = "@assignment.outer",
+                ["iv"] = "@assignment.inner",
+            }
+
+            for k, p in pairs(keymaps) do
+                vim.keymap.set({ "x", "o" }, k, function()
+                    require "nvim-treesitter-textobjects.select".select_textobject(p, "textobjects")
+                end)
+            end
+
+            require("nvim-treesitter-textobjects").setup(opts)
+        end
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        ft = { "vue", "html", "typescriptreact", "javascriptreact", "xml" },
+        opts = {},
+    },
+    {
+        "brenoprata10/nvim-highlight-colors",
+        ft = { "vue", "css", "less", "html", "typescriptreact", "javascriptreact", "dart" },
+        opts = {
+            render = "virtual",
             enable_named_colors = false,
             enable_tailwind = false,
-            virtual_symbol = '❆ ',
-        }
+            virtual_symbol = "❆ ",
+        },
     },
 }
