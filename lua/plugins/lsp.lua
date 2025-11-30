6a7a3b3fd0bbbc6e5eedf488c9411d4b5d5e2d18
LinHQ1999 <Hanqing_Lin1999@outlook.com>
2025-11-30 21:57:00 +0800

    fix: 解决 luasnip 大小写替换失效
    Windows build js_regexp 需要 $GIT/bin/sh.exe

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,6 +41,8 @@ return {
     {
         "L3MON4D3/LuaSnip",
         lazy = true,
+        version = "2.*",
+        build = "make install_jsregexp",
         dependencies = {
             { "rafamadriz/friendly-snippets" },
         },
